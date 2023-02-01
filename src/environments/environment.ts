9261da0e7769ed4ce690261937445ea2e382ae20
maksimry <maksimry@rabota.ua>
2023-02-01 14:43:39 +0800

    fierestore added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'ukraine-bali',
+    appId: '1:288832215498:web:09d18c61b940f7f8d41733',
+    storageBucket: 'ukraine-bali.appspot.com',
+    apiKey: 'AIzaSyB_vHWZFITVEXgSsAqoRCnshIsPDPKfnuA',
+    authDomain: 'ukraine-bali.firebaseapp.com',
+    messagingSenderId: '288832215498',
+    measurementId: 'G-5G5SSFRMCJ',
+  },
   production: true
 };
