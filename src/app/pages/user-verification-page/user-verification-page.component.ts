60ba42c5f074d9692b49c9600452f92fba8f9145
maksimry <maksimry@rabota.ua>
2023-02-09 16:36:46 +0800

    fetch data from firebase

--- src/app/pages/user-verification-page/user-verification-page.component.ts
+++ src/app/pages/user-verification-page/user-verification-page.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core'
+import {AngularFirestore} from '@angular/fire/compat/firestore'
+import {map} from 'rxjs'
+import {User} from '../../shared/interfaces/user'
 
 @Component({
   selector: 'app-user-verification-page',
   templateUrl: './user-verification-page.component.html',
   styleUrls: ['./user-verification-page.component.css']
 })
-export class UserVerificationPageComponent {
+export class UserVerificationPageComponent implements OnInit {
+  private readonly collectionName = 'demo-name-telegram'
 
+  constructor(private angularFirestore: AngularFirestore) {
+  }
+
+  ngOnInit() {
+
+    this.angularFirestore.collection(this.collectionName).get()
+      .pipe(map(docSnapshot => docSnapshot.docs.map(el=> el.data() as User)))
+      .subscribe((data: User[])  => {
+        console.log('ddd', data)
+      })
+  }
 }
